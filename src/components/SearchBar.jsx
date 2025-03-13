207f1099083930f6a10ef899ce49bbe108b56155
ThanhMTCE181198 <thanhmtce181198@fpt.edu.vn>
2025-03-13 07:24:45 +0700

    chú thích những gì mình thay đổi

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,14 @@
+function SearchBar({ searchTerm, setSearchTerm }) {
+    return (
+      <input
+        type="text"
+        placeholder="Tìm kiếm sản phẩm..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="form-control"
+      />
+    );
+  }
+  
+  export default SearchBar;
+  
